59474f2789270275b7a317decf51b32a7018eadc
Josh Torre <torre.jo@northeastern.edu>
2025-04-20 22:38:00 -0400

    added info file, cleaned up some of the names, and removed erroneous files

---
+++ info.rkt
@@ -0,0 +1,12 @@
+#lang info
+(define name "Fractalang")
+(define deps '("base"
+               "gui-lib"
+               "syntax-spec-v3"  
+               "htdp-lib"
+               "pict-lib"
+               "rackunit-lib"
+               "syntax-color-lib"
+               ))
+(define license 'MIT)
+(define scribblings '(("scribblings/main.scrbl" () (experimental) "Fractalang")))
